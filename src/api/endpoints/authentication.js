13f623a3108b23953860519f577471cf2d961957
Ian Swensson <ianswensson@Ians-MacBook-Pro.local>
2023-10-19 08:40:22 -0600

    password reset token page

--- src/api/endpoints/authentication.js
+++ src/api/endpoints/authentication.js
@@ -40,80 +40,8 @@ const signup = async ({ firstName, lastName, email, password }) => {
   }
 };
 
-const requestPasswordReset = async ({email}) => {
-  const payload = {
-    email,
-  };
-
-  try {
-    const res = await fetch(`${config.endpoint}/api/password_reset`, {
-      method: "POST",
-      headers: {"Content-Type": "application/json"},
-      body: JSON.stringify(payload),
-      credentials: 'include',
-    })
-    .then(response => response.json())
-    if (res.success) {
-      return {
-        error: false,
-        data: res.data,
-        message: res.message,
-      };
-    }
-    return {
-      error: true,
-      data: {},
-      message: res.message,
-    };
-  } catch (error) {
-    console.log(error);
-    return {
-      error: true,
-      data: {},
-      message: "Error connecting to the server, please try again.",
-    };
-  }
-}
-
-const resetPassword = async ({ token, password }) => {
-  const payload = {
-    password
-  };
-
-  try {
-    const res = await fetch(`${config.endpoint}/api/password_reset/${token}`, {
-      method: "POST",
-      headers: {"Content-Type": "application/json"},
-      body: JSON.stringify(payload),
-      credentials: 'include',
-    })
-    .then(response => response.json())
-    if (res.success) {
-      return {
-        error: false,
-        data: res.data,
-        message: res.message,
-      };
-    }
-    return {
-      error: true,
-      data: {},
-      message: res.message,
-    };
-  } catch (error) {
-    console.log(error);
-    return {
-      error: true,
-      data: {},
-      message: "Error connecting to the server, please try again.",
-    };
-  }
-}
-
 const authentication = {
   signup,
-  requestPasswordReset,
-  resetPassword
 };
 
 export default authentication;
