52c34b6794d2a01adbd398177e9fbfbb615e6394
Ian Swensson <ianswensson@Ians-MacBook-Pro.local>
2023-10-19 08:27:16 -0600

    login page

--- src/api/endpoints/authentication.js
+++ src/api/endpoints/authentication.js
@@ -1,44 +1,5 @@
 import config from "../../config/index";
 
-const login = async ({ email, password }) => {
-  // define payload
-  const payload = {
-    email,
-    password,
-  };
-  // fetch from url
-  try {
-    const res = await fetch(`${config.endpoint}/api/login`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(payload),
-      credentials: 'include',
-    })
-    .then((response) => response.json())
-    if (res.success) {
-      return {
-        error: false,
-        data: res.data,
-        message: res.message,
-      };
-    }
-    return {
-      error: true,
-      data: {},
-      message: res.message,
-    };
-  } catch (err) {
-    console.log(err);
-    return {
-      error: true,
-      data: {},
-      message: "Error connecting to the server, please try again.",
-    };
-  }
-};
-
 const signup = async ({ firstName, lastName, email, password }) => {
   // define payload
   const payload = {
@@ -150,7 +111,6 @@ const resetPassword = async ({ token, password }) => {
 }
 
 const authentication = {
-  login,
   signup,
   requestPasswordReset,
   resetPassword
